5a8f4ec9f5ccf1c622936d411e4dcea835e0be6f
Anttoam <ogagodev@gmail.com>
2025-04-19 16:55:32 +0900

    :+1: sqliteでの保存される時間をlocaltime(日本時間)に修正

--- src/persistence/drizzle/schema.ts
+++ src/persistence/drizzle/schema.ts
@@ -6,8 +6,12 @@ export const users = sqliteTable("users", {
 	username: text("username").notNull(),
 	email: text("email").notNull().unique(),
 	password: text("password").notNull(),
-	created_at: text("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
-	updated_at: text("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+	created_at: text("created_at")
+		.notNull()
+		.default(sql`(datetime('now', 'localtime'))`),
+	updated_at: text("updated_at")
+		.notNull()
+		.default(sql`(datetime('now', 'localtime'))`),
 });
 export type NewUser = typeof users.$inferInsert;
 export type User = typeof users.$inferSelect;
