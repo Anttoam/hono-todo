0ebb4b46d125bcc16486cb8b6d2ec09a40743de2
Anttoam <ogagodev@gmail.com>
2025-04-19 17:12:23 +0900

    :+1: loggerでparamsを削除

--- src/persistence/database.ts
+++ src/persistence/database.ts
@@ -1,5 +1,12 @@
+import type { Logger } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/d1";
 
+class MyLogger implements Logger {
+	logQuery(query: string): void {
+		console.log({ query });
+	}
+}
+
 export const getDb = (env: { DB: D1Database }) => {
-	return drizzle(env.DB, { logger: true });
+	return drizzle(env.DB, { logger: new MyLogger() });
 };
