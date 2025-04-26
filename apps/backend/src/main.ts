3c8881c662e69b8775c0db6c453d433d477be693
Anttoam <ogagodev@gmail.com>
2025-04-26 18:10:36 +0900

    :tada: dockerを導入

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import userRouter from "./view/controller/userController";
 
 const app = new Hono();
-
+app.get('/', (c) => c.text('Hello World'))
 app.route("/users", userRouter);
 
 export default app;
